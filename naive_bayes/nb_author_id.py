6792645e311735b1bebc590ab4d8b8e98391b135
hardik <hardiksinghnegi@gmail.com>
2018-07-18 04:03:28 -0700

    Add Email Classifier

---
+++ naive_bayes/nb_author_id.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+
+    Use a Naive Bayes Classifier to identify emails by their authors
+    
+    authors and labels:
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+
+
+#########################################################
+### your code goes here ###
+t_train = time()
+
+email_clf = GaussianNB()
+email_clf.fit(features_train,labels_train)
+
+print "training time:", round(time()-t_train, 3), "s"
+
+t_predict = time()
+
+clf_predictor = email_clf.predict(features_test)
+
+print "prediction time:", round(time()-t_predict, 3), "s"
+
+print accuracy_score(labels_test,clf_predictor)
+
+#########################################################
+
+
